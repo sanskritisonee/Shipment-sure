dfe43a36bd16fe57b72624e0fd45ad336923c35f
Sanskriti sonee <soneesanskriti@gmail.com>
2026-01-06 12:40:22 +0530

    Fix indentation in load_model function

--- app.py
+++ app.py
@@ -22,6 +22,7 @@ st.write("Predict whether a shipment will be **On-Time** or **Delayed** based on
 def load_model():
     model = joblib.load("shipment_delay_model.pkl")
     features = joblib.load("model_features.pkl")
+")
     return model, features
 
 model, feature_list = load_model()
